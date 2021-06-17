c0a71000ef88d333a02fbd1ea4b1d6bfb1cab976
susgupta <guptask.7@gmail.com>
2021-06-17 19:19:45 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-# vue-covid-tracker
+# Sushil Vue Js Covid Tracker App
+
+This is app demonstrating use of Vue.js components to build a simple Covid tracker app.  It is fetching live data stats from API service from:
+
+[Covid 19 API](https://covid19api.com/)
+
+To style the app [TailWind CSS](https://tailwindcss.com/) libraries are used, using simple utility-CSS classes from Tailwind CSS.
+
+## Technologies Used
+
+- HTML
+- Tailwind CSS 
+- JavaScript / Vue.js
+
+# Development and Build Guide
 
 ## Project setup
 ```
@@ -17,3 +31,12 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Real-Time Viewing For Web Version
+
+The application was publised using [GitHub Pages](https://pages.github.com/).
+The public 'dist' folder is root of hosted application 
+
+Here is link to view the application:
+
+[Sushil G Covid 19 Tracker app](https://susgupta.github.io/vue-covid-tracker/index.html)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      @click="clearCountryData"
+      v-if="stats.Country"
+      class="
+        bg=green-700
+        text-white
+        rounded
+        p-3
+        mt-10
+        focus:outline-none
+        hover:bg-green-600
+      "
+    >
+      Clear country
+    </button>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="Fetching data" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      //just call API
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = res.json();
+      return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      loading.value = true;
+      const data = await fetchCovidData();
+      title.value = "Global";
+      status.value = data.Global;
+      loading.value = false;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+
+    //populate data structure from API call
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
+};
 </script>
